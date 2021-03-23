6410b5ec9eee316b8acb415ed5e743629101e56d
LFN <liangfengning@foxmail.com>
2021-03-23 15:03:24 +0800

    使用 Svelte Kit 构建应用

---
+++ README.md
@@ -0,0 +1,13 @@
+# Laboratory
+
+一个实验室，在这里做一些有趣的东西。
+
+大概在 [https://lab.lifeni.life](https://lab.lifeni.life)。
+
+## 技术
+
+使用 Svelte Kit 构建页面，部署在 Netlify 上。
+
+## 协议相关
+
+本项目以 MIT License 开放源代码。
---
+++ src/routes/index.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import '../styles/global.less';
+</script>
+
+<svelte:head>
+  <title>实验室</title>
+</svelte:head>
+
+<main>
+  <h1>Hello, Laboratory</h1>
+</main>
+
+<style lang="less">
+  :root {
+    font-family: Overpass, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+      Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    position: relative;
+    width: 100%;
+    min-height: 100vh;
+    margin: 0 auto;
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: left;
+    user-select: none;
+  }
+
+  h1 {
+    color: #000;
+    font-size: 3.5rem;
+    font-weight: 800;
+    line-height: 1.35;
+    margin: 4rem auto;
+  }
+
+  @media (max-width: 648px) {
+    h1 {
+      font-size: 2.5rem;
+    }
+  }
+
+  @media (max-width: 480px) {
+    h1 {
+      font-size: 2rem;
+    }
+  }
+</style>
